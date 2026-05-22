9135f0a9deaf6a2a8aa462be368362f853482474
ayushdevelop <see.ayush.here.2@gmail.com>
2026-05-22 23:26:58 +0530

    lets move ahead with redis

--- redis/src/index.js
+++ redis/src/index.js
@@ -6,12 +6,12 @@ const app = express();
 
 const redis = new Redis(process.env.REDIS_CONNECTION || "redis://localhost:6379");
 
-app.get('/redis', async ({ req, res }) => {
+app.get('/redis', async (req, res) => {
     const reply = await redis.ping();
     res.json({ redis: reply });
 });
 
-app.get('/mongo', async ({ req, res }) => {
+app.get('/mongo', async (req, res) => {
     const url = process.env.MONGO_URL || 'mongodb://localhost:27017/mongo_playground'
 
     if (mongoose.connect.readyState === 0) {
